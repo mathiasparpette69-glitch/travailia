faab4fa2364cfb25214ca4dd447dc23f07cab3f9
mathiasparpette69-glitch <mathiasparpette69@gmail.com>
2026-08-03 21:41:39 +0200

    Update cv.py

--- backend/cv.py
+++ backend/cv.py
@@ -1,10 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 
 router = APIRouter()
 
 
-@router.get("/cv")
-def cv():
+@router.post("/cv")
+def ajouter_cv(
+    fichier: UploadFile = File(...)
+):
     return {
-        "message": "Gestion du CV candidat JobIA"
+        "message": "CV reçu avec succès",
+        "nom_fichier": fichier.filename
+    }
+
+
+@router.get("/cv")
+def voir_cv():
+    return {
+        "message": "CV du candidat JobIA"
     }
